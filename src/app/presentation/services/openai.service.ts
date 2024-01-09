007d4718022efec083daaeaff91e5166e0b5fa5b
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-09 10:01:22 -0500

    Servicio actualizado

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { orthographyUseCase } from '@use-cases/index';
+import { from } from 'rxjs';
 
 
 
 @Injectable({providedIn: 'root'})
 export class OpenAiService {
 
+
+  checkOrthography( prompt: string ) {
+    return from( orthographyUseCase(prompt) ) ;
+  }
 
 
 }
