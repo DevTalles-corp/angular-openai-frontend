864f1eeb891d11f60d7e274c2d15397e7ec4e28e
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-15 09:56:23 -0500

    Chat agregado para el asistente

--- src/app/presentation/pages/assistantPage/assistantPage.component.ts
+++ src/app/presentation/pages/assistantPage/assistantPage.component.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
-@Component( {
+@Component({
   selector: 'app-assistant-page',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './assistantPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-} )
-export default class AssistantPageComponent { }
+})
+export default class AssistantPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+  }
+}
